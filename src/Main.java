f6765b5c1d9c70c4570c913fc51144add0b7d98f
geetikamahajan <geetika00@gmail.com>
2019-12-05 16:10:32 -0500

    finished popv

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[ ] args) {
         String filePath = args[0];
+
         File file = new File(filePath);
         List<String> lines = new ArrayList<>();
         try {
--- src/PopvStmt.java
+++ src/PopvStmt.java
@@ -1,5 +1,15 @@
+import java.nio.ByteBuffer;
+
 public class PopvStmt extends Stmt {
     public void genCode(String[] tokens){
-
+        String var_name = tokens[1];
+        int[] value = symbolTable.get(var_name);
+        int mem_address = value[0];
+        //push mem address
+        String[] push_tokens = {"pushi", Integer.toString(mem_address)};
+        StatementFactory.getStatement("pushi").genCode(push_tokens);
+        // popv
+        byte [] popv_bytes = ByteBuffer.allocate(4).putInt(POPV).array();
+        mem.add(popv_bytes[3]);
     }
 }
